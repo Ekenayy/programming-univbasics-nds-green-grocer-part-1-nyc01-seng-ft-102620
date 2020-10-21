9c28e3426972e806f8ad72ba79ac92f21426b943
Ekene N <ekenayy@gmail.com>
2020-10-21 18:10:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,7 @@ end
   
   cart.each do |grocery_item|
     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    binding.pry
+    #binding.pry
     if current_item
       new_cart_index = 0
       new_cart.each do |new_cart_item|
@@ -27,6 +27,7 @@ end
         end
         new_cart_index += 1
       end
+      binding.pry 
     else
       grocery_item[:count] = 1
       new_cart << grocery_item
